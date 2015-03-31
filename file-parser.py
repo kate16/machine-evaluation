58dc82b30ce6052f5439f4928d2f06f0936d4729
Luke Carlson <jlukecarlson@gmail.com>
2015-03-30 23:35:48 -0400

    py script used to extract student-data from translations.tsv

---
+++ file-parser.py
@@ -0,0 +1,19 @@
+import sys
+#
+# This file is used to extract information for students
+# from the translations.tsv file
+#
+
+all_data = [pair.strip().split('\t') for pair in open("all-data/translations.tsv")]
+# keep column names
+titles = all_data[0]
+# strip column names from the data
+all_data = all_data[1:len(all_data)]
+
+for line in all_data:
+    # The three reference translations
+    ref_translations = line[2:5]
+    # The four worker translations
+    turk_translations = line[6:10]
+    # Print out the important section
+    sys.stdout.write("%s\n" % '\t'.join(turk_translations))
